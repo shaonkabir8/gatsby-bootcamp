94bffdc048424b60a4964709b274989b6213edfd
Shaonkabir <shaonkabir98@gmail.com>
2020-01-06 14:50:16 +0600

    Fetched data from Markdown files and display them on Blog Page

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,5 +19,20 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
   ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,39 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { graphql, useStaticQuery } from "gatsby"
 
-const blog = () => (
-  <Layout>
-    <h3> Blog Page </h3>
-    <h3> Contents are loading soon...</h3>
-  </Layout>
-)
+const Blog = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
 
-export default blog
+  return (
+    <Layout>
+      <h2> Blog Page </h2>
+      <h3>Recent Posts</h3>
+      <ol>
+        {data.allMarkdownRemark.edges.map(data => {
+          return (
+            <li key={data.node.frontmatter.title}>
+              <h2>{data.node.frontmatter.title}</h2>
+              <p>{data.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
+}
+
+export default Blog
