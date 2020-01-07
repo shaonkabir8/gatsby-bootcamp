c7fdd299e15e47f289acce817ca7bf8e0bef6d61
Shaonkabir <shaonkabir98@gmail.com>
2020-01-07 16:34:56 +0600

    Fixed ! Images are showing ini Browser from Contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 export const query = graphql`
 	query($slug: String!) {
-		contentfulBlogPost(slug: {eq: $slug}) {
+		contentfulBlogPost(slug: { eq: $slug }) {
 			title
 			publishedDate(formatString: "Do MMMM YYYY")
 			postContent {
@@ -15,12 +15,24 @@ export const query = graphql`
 	}
 `
 const Blog = props => {
+	const options = {
+		renderNode: {
+			"embedded-asset-block": node => {
+				const alt = node.data.target.fields.title["en-US"]
+				const url = node.data.target.fields.file["en-US"].url
+				return <img alt={alt} src={url} />
+			},
+		},
+	}
 
 	return (
 		<Layout>
 			<h3>{props.data.contentfulBlogPost.title}</h3>
 			<p>{props.data.contentfulBlogPost.publishedDate}</p>
-			{documentToReactComponents(props.data.contentfulBlogPost.postContent.json)}
+			{documentToReactComponents(
+				props.data.contentfulBlogPost.postContent.json,
+				options
+			)}
 		</Layout>
 	)
 }
